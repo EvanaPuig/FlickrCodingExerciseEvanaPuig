2c751c1b5f01610649b727d664097f4157279a31
Evana Puig <evanamp233@gmail.com>
2025-03-25 18:48:34 -0600

    Setting the final Photo model

--- VSCO-lite-swiftui/Models/Photo.swift
+++ VSCO-lite-swiftui/Models/Photo.swift
@@ -8,49 +8,28 @@
 import Foundation
 
 struct Photo: Codable, Identifiable {
-    let title: String
-    let url: URL?
     let id: String
-    let dateTaken: String
-    let height: Int?
-    let width: Int?
     let owner: String
-    let views: String
-    let license: String
-    @CodableValue var latitude: String // handle Flickr API returning an Int or a String
-    @CodableValue var longitude: String // handle Flickr API returning an Int or a String
-    @CodableValue var accuracy: String // handle Flickr API returning an Int or a String
+    let secret: String
+    let server: String
+    let title: String
     
     enum CodingKeys: String, CodingKey {
-        case title
-        case url = "url_z"
         case id = "id"
-        case dateTaken = "datetaken"
-        case height = "height_z"
-        case width = "width_z"
-        case owner = "ownername"
-        case views
-        case license
-        case latitude
-        case longitude
-        case accuracy
+        case owner = "owner_name"
+        case secret
+        case server
+        case title
     }
     
     // use as preview data for SwiftUI
     static var `default`: Photo {
         Photo(
-            title: "Flamingos By The Golden Sunset",
-            url: URL(string: "https://live.staticflickr.com/65535/50392963378_b2954d2021_z.jpg"),
             id: "50392963378",
-            dateTaken: "2017-08-01 09:14:37",
-            height: 427,
-            width: 640,
             owner: "126108137@N05",
-            views: "13375",
-            license: "0",
-            latitude: "43.462825",
-            longitude: "4.484825",
-            accuracy: "13"
+            secret: "b2954d2021",
+            server: "65535",
+            title: "Flamingos By The Golden Sunset"
         )
     }
 }
